e6b969be042a0604365f670b2816dd60b6e72287
Kevin King <kingnh@comcast.net>
2026-04-18 21:18:38 -0400

    add CinderShard and Cindercore

---
+++ common/src/main/java/io/drahlek/cinderstride/items/CinderCore.java
@@ -0,0 +1,12 @@
+package io.drahlek.cinderstride.items;
+
+import io.drahlek.dirigo.annotation.Item;
+
+@Item(id = "cinder_core")
+public class CinderCore extends net.minecraft.world.item.Item  {
+    static public final String NAME = "cinder_core";
+
+    public CinderCore(Properties properties) {
+        super(properties);
+    }
+}
