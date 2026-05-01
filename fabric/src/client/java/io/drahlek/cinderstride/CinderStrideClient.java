626202456ab4f7c17393ae94bcc02a29b9e9a0f6
Kevin King <kingnh@comcast.net>
2026-05-01 00:13:38 -0400

    fix heaerthfire flame texture

--- fabric/src/client/java/io/drahlek/cinderstride/CinderStrideClient.java
+++ fabric/src/client/java/io/drahlek/cinderstride/CinderStrideClient.java
@@ -1,9 +1,18 @@
 package io.drahlek.cinderstride;
 
+import io.drahlek.cinderstride.blocks.HearthFireBlock;
+import io.drahlek.dirigo.registrars.BlockRegistrar;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public class CinderStrideClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        Constants.LOG.info("{} Client Initialize", Constants.MOD_NAME);    }
+        Constants.LOG.info("{} Client Initialize", Constants.MOD_NAME);
+
+        BlockRenderLayerMap.putBlock(
+                BlockRegistrar.blocks.get(HearthFireBlock.NAME).get(),
+                ChunkSectionLayer.CUTOUT);
+     }
 }
