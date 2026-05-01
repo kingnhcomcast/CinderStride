1ba3b7511503f0fe5c577c3c86da771674f08475
Kevin King <kingnh@comcast.net>
2026-05-01 00:13:38 -0400

    fix heaerthfire flame texture

--- neoforge/src/client/java/io/drahlek/cinderstride/client/CinderStrideClient.java
+++ neoforge/src/client/java/io/drahlek/cinderstride/client/CinderStrideClient.java
@@ -1,9 +1,14 @@
 package io.drahlek.cinderstride.client;
 
 import io.drahlek.cinderstride.Constants;
+import io.drahlek.cinderstride.blocks.HearthFireBlock;
+import io.drahlek.dirigo.registrars.BlockRegistrar;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static io.drahlek.cinderstride.Constants.MOD_ID;
 
@@ -12,5 +17,17 @@ public final class CinderStrideClient {
     public CinderStrideClient(IEventBus modBus) {
         // Perform logic in that should only be executed on the physical client
         Constants.LOG.info("{} Client Initialize", Constants.MOD_NAME);
+
+        // Register client-only events
+        modBus.addListener(this::onClientSetup);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ItemBlockRenderTypes.setRenderLayer(
+                    BlockRegistrar.blocks.get(HearthFireBlock.NAME).get(),
+                    ChunkSectionLayer.CUTOUT
+            );
+        });
     }
 }
